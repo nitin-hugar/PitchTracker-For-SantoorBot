c5aa2e8ade5d353eb45d67fc540c1a71c0d95fb1
nitin-hugar <nhugar3@gatech.edu>
2021-11-25 18:39:28 -0500

    make notes

--- makenotes.py
+++ makenotes.py
@@ -1,9 +1,26 @@
 # convert onsets and f0 into notes 
 
-def makeNotes(f0Array, onsetTimes, silenceMask):
+# import libraries
+import numpy as np
+
+
+#Generate notes: 
+def makeNotes(pitchChroma, onsetTimes, silenceMask):
     
-    return notes
+    # find the pitch class from the chromagram:
+    pitch = np.zeros(pitchChroma.shape[1])
+    for block in pitchChroma.shape[1]:
+        pitch[block] = np.argmax(pitchChroma[:, block])
+    
+    
+
+    
+    
+
+        
+
+    
+
 
 
 # convert notes into midi
-
